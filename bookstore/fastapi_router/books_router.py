437519af3e07c4163b0283768b1aabbbc9d390a1
Mohamad-Hachem <mohamad27hachem@outlook.com>
2025-09-05 12:28:27 +0200

    adding models

--- bookstore/fastapi_router/books_router.py
+++ bookstore/fastapi_router/books_router.py
@@ -1,5 +1,7 @@
+from typing import List
 from fastapi import APIRouter
-
+from ..models import Book, BookResponse
+from pydantic import BaseModel
 books_router = APIRouter()
 
 @books_router.get("/books")
@@ -18,3 +20,29 @@ async def read_book(book_id: int):
         "title": "The Great Gatsby",
         "author": "F. Scott Fitzgerald"
     }
+
+@books_router.get("/books/booked/{book_id}")
+async def read_book(book_id: int):
+    return {
+        "book_id": book_id,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald"
+    }
+
+@books_router.get("/allbook", response_model=List[BookResponse])
+async def read_all_books():
+    return [{
+            "id": 1,
+            "title": "1984",
+            "author": "George Orwell"},
+            {
+            "id": 2,
+            "title": "The Great Gatsby",
+            "author": "F. Scott Fitzgerald",
+             },
+    ]
+@books_router.post("/book")
+async def create_book(book: Book):
+    return book
+
+
